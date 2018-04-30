7e8c3cc57fe1e82cba6e029001db2d90380ca81f
Michal Miciak <maq0297@gmail.com>
2018-04-30 14:59:38 +0200

    Adding CarServiceTests

--- DriverExpansesTracker.API/Startup.cs
+++ DriverExpansesTracker.API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DriverExpansesTracker.API.Filters;
+using DriverExpansesTracker.API.Infrastructure;
 using DriverExpansesTracker.Repository.Entities;
 using DriverExpansesTracker.Repository.Repositories;
 using DriverExpansesTracker.Services.Services;
@@ -96,24 +97,7 @@ namespace DriverExpansesTracker.API
                 app.UseDeveloperExceptionPage();
             }
 
-            AutoMapper.Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap<DriverExpansesTracker.Repository.Entities.User, DriverExpansesTracker.Services.Models.User.UserDto>();
-                cfg.CreateMap<DriverExpansesTracker.Services.Models.User.UserForCreationDto, DriverExpansesTracker.Repository.Entities.User>()
-                   .ForMember(dest=>dest.PasswordHash,opt=>opt.Ignore());
-
-                cfg.CreateMap<DriverExpansesTracker.Repository.Entities.Car, DriverExpansesTracker.Services.Models.Car.CarDto>();
-                cfg.CreateMap<DriverExpansesTracker.Services.Models.Car.CarForCreationDto, DriverExpansesTracker.Repository.Entities.Car>();
-
-                cfg.CreateMap<DriverExpansesTracker.Repository.Entities.Journey, DriverExpansesTracker.Services.Models.Journey.JourneyDto>();
-                cfg.CreateMap<DriverExpansesTracker.Services.Models.Journey.JourneyForCreationDto, DriverExpansesTracker.Repository.Entities.Journey>()
-                   .ForMember(dest=>dest.PassengerRoutes,opt=>opt.MapFrom(src=>src.Routes));
-
-                cfg.CreateMap<DriverExpansesTracker.Repository.Entities.PassengerRoute, DriverExpansesTracker.Services.Models.PassengerRoute.PassengerRouteDto>();
-                cfg.CreateMap<DriverExpansesTracker.Services.Models.PassengerRoute.PassengerRouteForCreationDto, DriverExpansesTracker.Repository.Entities.PassengerRoute>();
-
-                cfg.CreateMap<DriverExpansesTracker.Repository.Entities.Payment, DriverExpansesTracker.Services.Models.Payment.PaymentDto>();
-            });
+            AutoMapperConfiguration.Configure();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
---
+++ DriverExpansesTracker.Services.Tests/Fixtures/MapperFixture.cs
@@ -0,0 +1,19 @@
+﻿using DriverExpansesTracker.API.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DriverExpansesTracker.Services.Tests.Fixtures
+{
+    public class MapperFixture : IDisposable
+    {
+        public MapperFixture()
+        {
+            AutoMapperConfiguration.Configure();
+        }
+        public void Dispose()
+        {
+            
+        }
+    }
+}
